0f505f5f3ca79301eb4fbedce72959121e610812
FarhanZizz <iamfarhanaziz@gmail.com>
2022-11-11 19:42:15 +0600

    added service details api and reviews api

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 // MiddleWare
@@ -15,6 +15,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const serviceCollections = client.db('lens-knight').collection('services')
+        const reviewCollections = client.db('lens-knight').collection('reviews')
 
         app.get('/', async (req, res) => {
             const query = {}
@@ -25,6 +26,18 @@ async function run() {
         app.get('/services', async (req, res) => {
             const query = {}
             const cursor = serviceCollections.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+        app.get('/service/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await serviceCollections.findOne(query);
+            res.send(result)
+        })
+        app.get('/reviews', async (req, res) => {
+            const query = {}
+            const cursor = reviewCollections.find(query);
             const result = await cursor.toArray();
             res.send(result)
         })
