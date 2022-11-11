359e7c4836bf07ee26cd1d510ec12f66a8decb88
FarhanZizz <iamfarhanaziz@gmail.com>
2022-11-11 18:07:55 +0600

    added service preview api

--- index.js
+++ index.js
@@ -12,8 +12,21 @@ app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.d0hszsm.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        const serviceCollections = client.db('lens-knight').collection('services')
 
+        app.get('/', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollections.find(query).limit(3);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+    }
+    finally { }
+}
 
+run().catch(err => console.log(err))
 
 app.get('/', (req, res) => {
     res.send('server running');
